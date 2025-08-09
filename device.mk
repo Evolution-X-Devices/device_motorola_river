01979508d520e0fcf6c09aa473bd0700fcd20be5
Deivid21 <david.parra.ignacio@gmail.com>
2025-08-09 04:11:02 -0400

    river: Move sensors configs to vendor
    Change-Id: I1ad79f5b8bec480b7c0f2a0c50ec3eedac57f504

--- device.mk
+++ device.mk
@@ -74,11 +74,6 @@ PRODUCT_PACKAGES += \
     AvoidAppsInCutoutOverlay \
     NoCutoutOverlay
 
-# Sensors
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf \
-    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
-
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 28
 
